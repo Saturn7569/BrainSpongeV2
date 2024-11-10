1b2265792afde761f7c1ab8039c5e0e252c5fa40
Saturn7569 <143340840+Saturn7569@users.noreply.github.com>
2024-11-10 13:02:44 +0100

    Update interpreter.py

--- src/interpreter.py
+++ src/interpreter.py
@@ -3,3 +3,8 @@ class Interpreter:
         self.tokens = tokens
         self.mem = [0 for _ in range(255)]
         self.ptr = 0
+        self.pos = 0
+
+    def load_tokens(self, tok):
+        self.tokens = tok
+        self.pos = 0
